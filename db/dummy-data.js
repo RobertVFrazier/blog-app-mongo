2592cc1626f2e865befe19e0883749fbb24512e4
Robert Frazier <robert.v.frazier@gmail.com>
2017-11-24 16:06:58 -0500

    My dummy-data.js replacing the one Chris made.

--- db/dummy-data.js
+++ db/dummy-data.js
@@ -1,52 +1,57 @@
-'use strict';
+"use strict";
 
 const data = [
   {
     "id": 1001,
-    "title": "What the government doesn''t want you to know about cats",
-    "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    "title": "What the World Would Be Like If Full-Stack Web Dev Didn''t Exist",
+    "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
   },
   {
     "id": 1002,
-    "title": "The most boring article about cats you''ll ever read",
-    "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    "title": "How Kitten Videos Can Keep You from Full-Stack Web Dev",
+    "content": "Senectus et netus et malesuada fames ac turpis egestas. Posuere lorem ipsum dolor sit amet. Viverra ipsum nunc aliquet bibendum. Nunc mi ipsum faucibus vitae aliquet nec ullamcorper. Ultrices mi tempus imperdiet nulla. A arcu cursus vitae congue mauris rhoncus."
   },
   {
     "id": 1003,
-    "title": "7 things lady gaga has in common with cats",
-    "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    "title": "Why Do People Think Full-Stack Web Dev Is a Good Idea?",
+    "content": "Risus nec feugiat in fermentum. Elit duis tristique sollicitudin nibh. Vulputate enim nulla aliquet porttitor lacus luctus accumsan tortor. Nunc mattis enim ut tellus elementum sagittis. Etiam dignissim diam quis enim lobortis scelerisque fermentum dui faucibus."
   },
   {
     "id": 1004,
-    "title": "The most incredible article about cats you''ll ever read",
-    "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    "title": "What the Beatles Could Learn from Full-Stack Web Dev",
+    "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ornare lectus sit amet est placerat in egestas. Scelerisque purus semper eget duis at. Adipiscing at in tellus integer feugiat."
   },
   {
     "id": 1005,
-    "title": "10 ways cats can help you live to 100",
-    "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    "title": "18 Reasons You Should be Talking About Full-Stack Web Dev",
+    "content": "Adipiscing elit duis tristique sollicitudin. Sed odio morbi quis commodo odio aenean sed adipiscing. Etiam non quam lacus suspendisse faucibus. Vel eros donec ac odio tempor. Proin fermentum leo vel orci porta non."
   },
   {
     "id": 1006,
-    "title": "9 reasons you can blame the recession on cats",
-    "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    "title": "Why Holden Caulfield Thinks Full-Stack Web Dev is Phony",
+    "content": "Est ullamcorper eget nulla facilisi etiam dignissim diam quis enim. Neque convallis a cras semper auctor neque. Sed vulputate odio ut enim blandit volutpat maecenas volutpat. Aenean euismod elementum nisi quis eleifend."
   },
   {
     "id": 1007,
-    "title": "10 ways marketers are making you addicted to cats",
-    "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    "title": "The 15 Worst Full-Stack Web Devs in History",
+    "content": "Bibendum ut tristique et egestas quis ipsum suspendisse ultrices. Montes nascetur ridiculus mus mauris vitae ultricies. Vulputate enim nulla aliquet porttitor lacus luctus. Quis auctor elit sed vulputate. Duis tristique sollicitudin nibh sit amet commodo."
   },
   {
     "id": 1008,
-    "title": "11 ways investing in cats can make you a millionaire",
-    "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    "title": "9 Great Articles About Full-Stack Web Dev",
+    "content": "Fusce ut placerat orci nulla pellentesque dignissim enim sit. Id diam vel quam elementum pulvinar etiam non. Mi proin sed libero enim. Ut etiam sit amet nisl purus in mollis nunc. Donec adipiscing tristique risus nec. Tristique senectus et netus et malesuada fames ac."
   },
   {
     "id": 1009,
-    "title": "Why you should forget everything you learned about cats",
-    "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    "title": "7 Reasons Full-Stack Web Devs Could Help Win Top Chef",
+    "content": "Tellus elementum sagittis vitae et leo duis ut. Quam elementum pulvinar etiam non quam lacus suspendisse faucibus interdum. Risus pretium quam vulputate dignissim suspendisse in est ante. Lectus vestibulum mattis ullamcorper velit sed ullamcorper."
+  },
+  {
+    "id": 1010,
+    "title": "13 Ways Full-Stack Web Dev Can Find You the Love of Your Life",
+    "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ornare lectus sit amet est placerat in egestas. Scelerisque purus semper eget duis at. Adipiscing at in tellus integer feugiat."
   }
 ];
-data.nextVal = 1010;
+data.nextVal = 1011;
 
 module.exports = data;
